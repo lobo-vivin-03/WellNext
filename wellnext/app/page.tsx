e598181908b4b34e08cca9356df6a9840b801c70
lobo-vivin-03 <vivinlobo5@gmail.com>
2024-09-06 01:11:29 +0530

    qwertyuio

---
+++ wellnext/app/page.tsx
@@ -0,0 +1,43 @@
+import PatientForm from "@/components/forms/PatientForm";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen max-h-screen">
+      
+
+
+      <section className="remove-scroller container my-auto">
+        <div className="sub-container max-w-[496px] ">
+          <Image
+          src="/assets/icons/logo.png" 
+          height={200}
+          width={200}
+          alt="logo"
+          className="mb-12 h-12 w-fit "
+          />
+
+          <PatientForm/>
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xltext-left">
+            © 2024 WellNext
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+      <Image
+      src="/assets/images/onboarding-img.png"
+      height={1000}
+      width={1000}
+      alt="patient"
+      className="size-img max-w-[50%]"
+      />
+    </div>
+  );
+}
